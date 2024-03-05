502d7fb3aa11242a77ca2300151e6ef8a222da95
Jason Hill <jason@hilly.me>
2024-03-05 18:37:53 +1000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import Greet from "./components/Greet.vue";
+</script>
+
+<template>
+  <div class="container">
+    <div class="row">
+      <button class="btn">1</button>
+      <button class="btn">2</button>
+      <button class="btn">3</button>
+      <button class="btn">4</button>
+    </div>
+    <Greet />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
