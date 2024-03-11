41ae0b19ae9d53cee560112df1febd4d77e18d4a
Jason Hill <jason@hilly.me>
2024-03-11 16:39:30 +1000

    Moving Blower fan control into own component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,11 @@
 <script setup>
 import Temperature from "./components/Temerature.vue";
-import {invoke} from "@tauri-apps/api/tauri";
-
-async function setFanSpeed(speed) {
-  await invoke("set_fan_speed", { speed });
-}
+import BlowerControl from "./components/BlowerControl.vue";
 </script>
 
 <template>
-  <div class="container">
-    <div class="row">
-      <button class="btn mr-10 h-32 w-32" @click="setFanSpeed(0)">Off</button>
-      <button class="btn mr-10 h-32 w-32" @click="setFanSpeed(1)">Low</button>
-      <button class="btn mr-10 h-32 w-32" @click="setFanSpeed(2)">Med</button>
-      <button class="btn h-32 w-32" @click="setFanSpeed(3)">High</button>
-    </div>
-    <Temperature />
-  </div>
+  <BlowerControl />
+  <Temperature />
 </template>
 
 <style scoped>
---
+++ src/components/BlowerControl.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {invoke} from "@tauri-apps/api/tauri";
+
+async function setFanSpeed(speed) {
+  await invoke("set_fan_speed", { speed });
+}
+</script>
+
+<template>
+  <div class="row">
+    <button class="btn mr-10 h-32 w-32" @click="setFanSpeed(0)">Off</button>
+    <button class="btn mr-10 h-32 w-32" @click="setFanSpeed(1)">Low</button>
+    <button class="btn mr-10 h-32 w-32" @click="setFanSpeed(2)">Med</button>
+    <button class="btn h-32 w-32" @click="setFanSpeed(3)">High</button>
+  </div>
+</template>
